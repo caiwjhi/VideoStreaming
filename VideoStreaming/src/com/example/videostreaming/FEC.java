81d13a89e23449c8abba8b91c81d8537ca575ddf
caiwjhi <caiwj425@163.com>
2018-01-25 13:45:33 +0800

    fec in android

--- VideoStreaming/src/com/example/videostreaming/FEC.java
+++ VideoStreaming/src/com/example/videostreaming/FEC.java
@@ -1,3 +1,4 @@
+package com.example.videostreaming;
 import java.util.Arrays;
 
 public class FEC {
@@ -27,57 +28,84 @@ public class FEC {
 	}
 	
 	public void encode(byte[][] D, byte[][] C, int len, int offset) {
-		int tmp;
-		for (int digit = offset; digit < len+offset; digit++){
-			for (int i = 0; i < N; i++) {
-				tmp = 0;
-				for (int j = 0; j < M; j++){
-					 tmp += unsigned(D[j][digit]) * V[i][j];
-					 tmp %= 256;
-				}
-				C[i][digit] = (byte)tmp;
+		short tmp;
+		for (int i = offset; i < len+offset; i++){
+			tmp = 0;
+			for (int j = 0; j < M; j++){
+				 tmp += unsigned(D[j][i]);
 			}
+			C[0][i] = (byte)tmp;
 		}
+//		int tmp;
+//		for (int digit = offset; digit < len+offset; digit++){
+//			for (int i = 0; i < N; i++) {
+//				tmp = 0;
+//				for (int j = 0; j < M; j++){
+//					 tmp += unsigned(D[j][digit]) * V[i][j];
+//					 tmp %= 256;
+//				}
+//				C[i][digit] = (byte)tmp;
+//			}
+//		}
 	}
 
 	public void decode(byte[][] D, byte[][] C, int[] ready, int len) {
-		int num = 0;
-		for (int i = 0; i < M; i++) {
-			if (ready[i] != 0) {
-				num ++;
-			}
-		}
-		if (num == M) // no need to decode
-			return;
-		
-		// get encoded from existing data blocks
-		int n = M - num;
-		int[][] R = new int[n][len];
-		int tmp = 0;
-		int pos = 0;
-		for (int digit = 0; digit < len; digit++){
-			for (int i = 0; i < N; i++) {
-				if (ready[M+i] == 0)
-					continue;
-				tmp = 0;
-				for (int j = 0; j < M; j++){
-					if (ready[j] != 0) {
-						tmp += unsigned(D[j][digit]) * V[pos][j];
-						tmp %= 256;
-					}
-				}
-				R[pos][digit] = (unsigned(C[pos][digit]) - tmp + 256) % 256;
-				pos ++;
-			}
-		}
-		
-		// solve matrix V' * D' = R
+		int num = -1;
 		for (int i = 0; i < M; i++) {
 			if (ready[i] == 0) {
-				System.arraycopy(R[0], 0, D[i], 0, len);
-				break;
+				num = i;
 			}
 		}
+		if (num == -1) {
+			return;
+		}
+		short tmp;
+		for (int i = 0; i < len; i++){
+			tmp = unsigned(C[0][i]);
+			for (int j = 0; j < M; j++){
+				if (j != num){
+					tmp -= unsigned(D[j][i]);
+				}
+			}
+			D[num][i] = (byte)tmp;
+		}
+//		int num = 0;
+//		for (int i = 0; i < M; i++) {
+//			if (ready[i] != 0) {
+//				num ++;
+//			}
+//		}
+//		if (num == M) // no need to decode
+//			return;
+//		
+//		// get encoded from existing data blocks
+//		int n = M - num;
+//		byte[][] R = new byte[n][len];
+//		int tmp = 0;
+//		int pos = 0;
+//		for (int digit = 0; digit < len; digit++){
+//			for (int i = 0; i < N; i++) {
+//				if (ready[M+i] == 0)
+//					continue;
+//				tmp = 0;
+//				for (int j = 0; j < M; j++){
+//					if (ready[j] != 0) {
+//						tmp += unsigned(D[j][digit]) * V[pos][j];
+//						tmp %= 256;
+//					}
+//				}
+//				R[pos][digit] = (byte) ((unsigned(C[pos][digit]) - tmp + 256) % 256);
+//				pos ++;
+//			}
+//		}
+//		
+//		// solve matrix V' * D' = R
+//		for (int i = 0; i < M; i++) {
+//			if (ready[i] == 0) {
+//				System.arraycopy(R[0], 0, D[i], 0, len);
+//				break;
+//			}
+//		}
 	}
 	
 	public void print(byte[][] x) {
